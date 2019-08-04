a038da38c34f2a5784c470891a1322d4f2159d65
iiSaLMaN <slmanarendo1950@gmail.com>
2019-08-04 19:26:25 +0300

    Add tests for leaderboard scores and scrolling

--- osu.Game.Tests/Visual/Gameplay/TestSceneInGameLeaderboard.cs
+++ osu.Game.Tests/Visual/Gameplay/TestSceneInGameLeaderboard.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using NUnit.Framework;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Game.Online.Leaderboards;
+using osu.Game.Scoring;
 using osu.Game.Screens.Play;
 using osu.Game.Users;
 using osuTK;
@@ -72,14 +74,64 @@ namespace osu.Game.Tests.Visual.Gameplay
             AddAssert("is player 2 position #3", () => leaderboard.CheckPositionByUsername("Player 2", 3));
         }
 
+        private List<ScoreInfo> setLeaderboard(int scoreCount, bool onlineScope)
+        {
+            var scores = new List<ScoreInfo>();
+            for (int i = 2; i < scoreCount + 2; i++)
+                scores.Add(new ScoreInfo { UserString = $"Player {i}", TotalScore = 1000000 * i });
+
+            AddStep($"set leaderboard {(onlineScope ? "online" : "offline")}-{scoreCount}", () => leaderboard.Leaderboard = new TestLeaderboard { Scores = scores, SetOnlineScope = onlineScope });
+
+            return scores.OrderByDescending(s => s.TotalScore).ToList();
+        }
+
+        [Test]
+        public void TestScoresScrolling()
+        {
+            var scores = setLeaderboard(7, false);
+
+            void isScrolled(bool down)
+            {
+                AddAssert($"{(down ? "no " : "")}player position #4", () => leaderboard.PositionExists(4) != down);
+                AddAssert($"{(down ? "" : "no ")}player position #8", () => leaderboard.PositionExists(8) == down);
+            }
+
+            isScrolled(true);
+
+            AddStep("set player above player 4", () => playerScore.Value = scores[4].TotalScore + 500000);
+            isScrolled(false);
+
+            AddStep("set player below player 2", () => playerScore.Value = scores[6].TotalScore - 500000);
+            isScrolled(true);
+        }
+
+        [Test]
+        public void TestDeclareNewPosition()
+        {
+            AddStep("set player position null", () => leaderboard.PlayerPosition = null);
+
+            var scores = setLeaderboard(50, true);
+
+            AddStep("set player score below last", () => playerScore.Value = 1000000);
+            AddAssert("no position declared for player", () => leaderboard.PlayerPosition == null);
+
+            setLeaderboard(50, false);
+
+            AddStep("set player score below last", () => playerScore.Value = 1000000);
+            AddAssert("new position declared for player", () => leaderboard.PlayerPosition == scores.Count + 1);
+        }
+
         private class TestInGameLeaderboard : InGameLeaderboard
         {
-            public void ClearScores() => ScoresContainer.RemoveAll(s => s.User.Username != PlayerUser.Username);
             public int? PlayerPosition
             {
                 get => PlayerScoreItem.ScorePosition;
                 set => PlayerScoreItem.ScorePosition = value;
             }
+
+            public new void ClearScores() => base.ClearScores();
+
+            public bool PositionExists(int position) => ScoresContainer.Any(s => s.ScorePosition == position);
 
             public bool CheckPositionByUsername(string username, int? estimatedPosition)
             {
@@ -89,6 +141,18 @@ namespace osu.Game.Tests.Visual.Gameplay
             }
 
             public void AddDummyPlayer(BindableDouble currentScore, string username) => ScoresContainer.AddRealTimePlayer(currentScore, new User { Username = username });
+        }
+
+        private class TestLeaderboard : ILeaderboard
+        {
+            public IEnumerable<ScoreInfo> Scores { get; set; }
+
+            public bool SetOnlineScope;
+            public bool IsOnlineScope => SetOnlineScope;
+
+            public void RefreshScores()
+            {
+            }
         }
     }
 }
